042eb917271a2db33b2151a87367c66aad63814a
nickd <nickdiaz58@gmail.com>
2021-08-03 20:42:05 -0400

    Initial file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Number Converter</h1>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
